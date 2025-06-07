ef05df0d393caa8c454bd662cd40624cd55854a9
Monya <isadom@mail.ru>
2025-06-07 16:20:48 +0400

    ...

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1300);
+     //   while (true){
+            Socket socket = serverSocket.accept();
+            ServerReadThread thread = new ServerReadThread(socket);
+
+//            while (!thread.isAlive()){} //это лишнее с этим циклом поток вечно висит даже если клеинт отключился
+            socket.close();
+     //   }
+
+//        serverSocket.close();
+
+
+
+
+//        OutputStream outStream = socket.getOutputStream();
+//        PrintStream printStream = new PrintStream(outStream);
+//        printStream.println("Hello!");
+//        socket.close();
+//        serverSocket.close();
+    }
+
+
 }
--- src/main/java/ServerReadThread.java
+++ src/main/java/ServerReadThread.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.Socket;
 
-public class ServerReadThread {
+public class ServerReadThread extends Thread {
+    Socket socket;
+
+    public ServerReadThread(Socket socket) {
+        this.socket = socket;
+        this.run();
+    }
+
+    @Override
+    public void run() {
+        try {
+                       BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String inLine;
+            OutputStream outStream = socket.getOutputStream();
+            PrintStream printStream = new PrintStream(outStream);
+            while ((inLine = reader.readLine()) != null) {
+                printStream.println("(" + socket.getPort() + ") " + inLine);
+//                if (reader.readLine().equals("exit")) this.interrupt();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
